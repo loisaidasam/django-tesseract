2d6f0cd6bd2ad4d72c69f5351150d66c45a05a84
sam <sam.sandberg@zemanta.com>
2012-08-07 04:19:01 +0200

    A couple of small template changes

--- djtesseract/web/views.py
+++ djtesseract/web/views.py
@@ -10,13 +10,12 @@ def index(request):
 	return render_to_response('index.html', context, context_instance=RequestContext(request))
 
 def ocr(request):
-	context = {
-		'tab_name': 'ocr',
-		'ocr_result': None,
-	}
+	context = {'tab_name': 'ocr'}
+	
 	if request.method == 'POST':
 		form = OCRForm(request.POST, request.FILES)
 		if form.is_valid():
+			context['got_ocr_result'] = True
 			context['ocr_result'] = handle_uploaded_file(request.FILES['img_file'])
 	else:
 		form = OCRForm()
