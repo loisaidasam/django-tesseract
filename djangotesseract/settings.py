cf0a581118140c42dcfb84f0171bf195743d30aa
sam <sam.sandberg@zemanta.com>
2012-08-11 06:35:27 +0200

    added 404 and 500 templates

--- djangotesseract/settings.py
+++ djangotesseract/settings.py
@@ -113,6 +113,7 @@ TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
+	next_to_this_file('templates'),
 )
 
 INSTALLED_APPS = (
